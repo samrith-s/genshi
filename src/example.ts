4165aded9eb30382007db176d4e266ffe89d27e7
Samrith Shankar <samrith.298@gmail.com>
2024-05-08 15:44:49 +0530

    chore: simplify example

--- src/example.ts
+++ src/example.ts
@@ -3,25 +3,15 @@ import { Store } from "./Store";
 /**
  * Example
  */
-const store = new Store({
-  count: 0,
-});
+const store = new Store(0);
 
 const increment = store.action<number>("increment", ({ state, payload }) => {
-  // console.log("## incrementing");
-  return {
-    count: state.count + payload,
-  };
+  return state + payload;
 });
 
 const plusMinus = store.action<boolean>("plus-minus", ({ state, payload }) => {
-  console.log("## state in +/-", state);
-  return {
-    count: state.count + (!!payload ? 1 : -1),
-  };
+  return state + (!!payload ? 1 : -1);
 });
-
-console.log("### state before", store.getState());
 
 const get = store.effect("data", ({ dispatch }) => {
   dispatch(increment, 10);
@@ -40,18 +30,4 @@ store.dispatch(get);
 store.dispatch(increment, 10);
 store.dispatch(plusMinus, Math.random() > 0.5);
 
-console.log("#history:");
-console.table(
-  store.history().map((history) => ({
-    type: history.type,
-    action: history.name,
-    payload: history.payload ?? NaN,
-    previousState: history.previousState,
-    currentState: history.currentState,
-    ...(history.source
-      ? { source: `${history.source.type} -> ${history.source.name}` }
-      : {}),
-  }))
-);
-
-console.log("### state after", store.getState());
+store.printHistory();
