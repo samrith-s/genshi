c7bdf0774c71cd282ce4b75f3f7df597ec63020e
Samrith Shankar <samrith.298@gmail.com>
2024-12-28 16:01:07 +0530

    feat: concepts, guides done

--- docs/astro.config.ts
+++ docs/astro.config.ts
@@ -2,6 +2,7 @@
 import react from "@astrojs/react";
 import starlight from "@astrojs/starlight";
 import tailwind from "@astrojs/tailwind";
+
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
