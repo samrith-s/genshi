06062d3087d21802e4c1213e5a1b45da7b23075d
Samrith Shankar <samrith.298@gmail.com>
2025-01-16 22:59:49 +0530

    chore: add typecheck and repair manifest files

--- scripts/shell/resolve-root.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# This just stores the scripts, doesn't run it
-export ROOT=$(readlink -f $PWD/../../)
-export NODE_MODULES = $(readlink -f $ROOT/node_modules)
-export NODE_MODULES_BIN = $(readlink -f $NODE_MODULES/.bin)
-
--- scripts/shell/typecheck.sh
+++ scripts/shell/typecheck.sh
@@ -4,8 +4,4 @@ export ROOT=$(readlink -f $PWD/../../);
 export NODE_MODULES=$(readlink -f $ROOT/node_modules);
 export NODE_MODULES_BIN=$(readlink -f $NODE_MODULES/.bin);
 
-echo $ROOT
-echo $NODE_MODULES
-echo $NODE_MODULES_BIN
-
-$NODE_MODULES_BIN/eslint --ignore-path $ROOT/.eslintignore $@;
+$NODE_MODULES_BIN/tsc --checkJs false -p ./tsconfig.check.json;
